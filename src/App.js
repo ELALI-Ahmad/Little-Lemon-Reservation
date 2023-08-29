c88830c69c3a29f1a25a1f941a80b17880a60813
Ahmad EL ALI <elali.ahm@gmail.com>
2023-08-29 23:11:35 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Routes
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import TopContainer from './components/TopContainer';
+import MenuContainer from './components/MenuContainer'
+import ReservationForm from './components/Reservation/ReservationForm';
+import ConfirmationContainer from './components/Reservation/ConfirmationContainer';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <TopContainer />
+        <Routes>
+          <Route path="/reservation" component={ReservationForm} />
+          <Route path="/confirmation" component={ConfirmationContainer} />
+        </Routes>
+        <MenuContainer />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
